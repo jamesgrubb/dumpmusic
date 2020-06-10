6fc6da3a32645cf74f71d7dcae27282eb7516a98
James <jamesgrubb@mac.com>
2020-06-10 15:32:57 +0100

    Added TrackLenght

--- www/src/components/Single/Single.js
+++ www/src/components/Single/Single.js
@@ -3,29 +3,42 @@ import { Link } from "gatsby"
 
 const Single = ({ name, tracks }) => {
   const [duration, setDuration] = useState([])
-  const audioRef = useRef(null)
-  console.log("Single -> ref", audioRef.current)
-
-  function getTrackLength(track) {
-    track.addEventListener("loadedmetadata", function () {
-      setDuration(track.duration)
-    })
+  const audioRef = useRef([new Array(Number(tracks.length))])
+  function fmtMSS(s) {
+    s = Math.floor(s)
+    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
   }
+
   useEffect(() => {
-    getTrackLength(audioRef.current)
-  }, [getTrackLength])
-  console.log(duration)
+    function getTrackLength(track) {
+      track.addEventListener("loadedmetadata", function () {
+        for (let i = 0; i <= tracks.length; i++) {
+          const seconds = track.duration
+          setDuration([...duration, fmtMSS(seconds)])
+        }
+      })
+    }
+
+    for (let i = 0; i < audioRef.current.length; i++) {
+      getTrackLength(audioRef.current[i])
+    }
+  }, [duration, tracks.length])
+
   return (
     <>
       <h2>{name}</h2>
 
       <ul>
-        {tracks.map(track => (
+        {tracks.map((track, i) => (
           <li key={track._id}>
             <div>
               <strong>{track.name}</strong>
-              <audio ref={audioRef} src={track.file} controls />
-              <i>{duration}</i>
+              <audio
+                ref={el => (audioRef.current[i] = el)}
+                src={track.file}
+                controls
+              />
+              <i>{duration[i]}</i>
             </div>
           </li>
         ))}
