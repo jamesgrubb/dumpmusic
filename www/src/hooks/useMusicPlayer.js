5dbd4f22677e4b286699c53762717bc8eca50f1f
James <jamesgrubb@mac.com>
2020-06-12 18:03:21 +0100

    Cleanup hook

--- www/src/hooks/useMusicPlayer.js
+++ www/src/hooks/useMusicPlayer.js
@@ -3,12 +3,12 @@ import { MusicPlayerContext } from "../context/MusicPlayerContext"
 
 const useMusicPlayer = () => {
   const [state, setState] = useContext(MusicPlayerContext)
-
+  // Convert number to minutes and seconds
   function fmtMSS(s) {
     s = Math.floor(s)
     return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
   }
-
+  // Get the length of the audio track
   function makeAudio(tracks) {
     let audioArr = new Array()
     tracks.forEach(({ file }) => {
@@ -30,46 +30,6 @@ const useMusicPlayer = () => {
       })
     })
   }
-
-  //   function makeAudio(tracks) {
-  //     const ARR = new Array()
-  //     for (let i = 0; i <= tracks.length; i++) {
-  //       const au = document.createElement("audio")
-  //       ARR.push(au)
-  //     }
-  //     console.log("makeAudio -> ARR", ARR)
-  //     tracks.forEach((track, i) => {
-  //       //   ARR[i] = document.createElement("audio")
-  //       //   console.log("ARR is " + ARR[i])
-  //       ARR[i].src = track.file
-  //       ARR[i].addEventListener("loadedmetadata", function () {
-  //         setState(state => ({
-  //           ...state,
-  //           tracks: state.tracks.concat(fmtMSS(ARR[i].duration)),
-  //         }))
-  //       })
-  //     })
-  //   }
-  //   //   function makeAudio(tracks) {
-  //     for (let i = 0; i <= tracks.length; i++) {
-  //       const auARR = new Array(tracks.length)
-  //       if (tracks.length) {
-  //         auARR.push(tracks[i])
-  //       }
-  //       console.log(auARR)
-  //     }
-  //   }
-
-  //) for (let i = 0; i <= tracks.length; i++) {
-  //   console.log(track.audioPlayer)
-  // }
-
-  // tracks.map((track, i) =>
-  // console.log("getTrackLength -> track, i", track, i)
-
-  //   state.audioPlayer[i].addEventListener("loadedmetadata", function () {
-  //     const seconds = state.audioPlayer[i].duration
-  //     setState(state => ({ ...state, duration: fmtMSS(seconds) }))
 
   function playTrack(index) {
     if (index === state.currentTrackIndex) {
