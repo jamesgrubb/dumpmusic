cb04bd3d4235d7a0d51bf1409af49bcb2447ea26
James <jamesgrubb@mac.com>
2020-06-12 17:54:40 +0100

    clean up single.js

--- www/src/components/Single/Single.js
+++ www/src/components/Single/Single.js
@@ -7,38 +7,19 @@ import useMusicPlayer from "../../hooks/useMusicPlayer"
 const Single = ({ name, tracks }) => {
   const {
     stopPlay,
-    togglePlay,
     playTrack,
     currentTrackName,
-    trackList,
     isPlaying,
     currentTrackIndex,
     makeAudio,
   } = useMusicPlayer()
-  const [duration, setDuration] = useState([])
+
   const [state, setState] = useContext(MusicPlayerContext)
-  // const audioRef = useRef([new Array(Number(trackList.length))])
-  // function fmtMSS(s) {
-  //   s = Math.floor(s)
-  //   return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
-  // }
 
   useEffect(() => {
     stopPlay()
     makeAudio(tracks)
     setState(state => ({ ...state, tracks: tracks, currentTrackIndex: null }))
-    // function getTrackLength(track) {
-    //   track.addEventListener("loadedmetadata", function () {
-    //     for (let i = 0; i <= tracks.length; i++) {
-    //       const seconds = track.duration
-    //       setDuration([...duration, fmtMSS(seconds)])
-    //     }
-    //   })
-    // }
-    // getTrackLength(tracks)
-    // for (let i = 0; i < audioRef.current.length; i++) {
-    //   getTrackLength(audioRef.current[i])
-    // }
   }, [])
 
   console.log("Single -> state", state)
