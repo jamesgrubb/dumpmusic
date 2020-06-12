aa6332a4069e85f56d363dab7a2eaa7c16173916
James <jamesgrubb@mac.com>
2020-06-12 11:30:22 +0100

    fixing track duration

--- www/src/components/Single/Single.js
+++ www/src/components/Single/Single.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState, useContext } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import { Link } from "gatsby"
 import { MusicPlayerContext } from "../../context/MusicPlayerContext"
 import MusicPlayerProvider from "../../context/MusicPlayerContext"
@@ -6,57 +6,65 @@ import useMusicPlayer from "../../hooks/useMusicPlayer"
 
 const Single = ({ name, tracks }) => {
   const {
+    stopPlay,
     togglePlay,
     playTrack,
     currentTrackName,
     trackList,
+    isPlaying,
+    currentTrackIndex,
+    makeAudio,
   } = useMusicPlayer()
   const [duration, setDuration] = useState([])
   const [state, setState] = useContext(MusicPlayerContext)
-  const audioRef = useRef([new Array(Number(tracks.length))])
-  function fmtMSS(s) {
-    s = Math.floor(s)
-    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
-  }
+  // const audioRef = useRef([new Array(Number(trackList.length))])
+  // function fmtMSS(s) {
+  //   s = Math.floor(s)
+  //   return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
+  // }
 
   useEffect(() => {
-    setState(state => ({ ...state, tracks: tracks }))
-    function getTrackLength(track) {
-      track.addEventListener("loadedmetadata", function () {
-        for (let i = 0; i <= tracks.length; i++) {
-          const seconds = track.duration
-          setDuration([...duration, fmtMSS(seconds)])
-        }
-      })
-    }
-
-    for (let i = 0; i < audioRef.current.length; i++) {
-      getTrackLength(audioRef.current[i])
-    }
-  }, [duration, tracks.length, setState, tracks])
+    makeAudio(tracks)
+    setState(state => ({ ...state, tracks: tracks, currentTrackIndex: null }))
+    // function getTrackLength(track) {
+    //   track.addEventListener("loadedmetadata", function () {
+    //     for (let i = 0; i <= tracks.length; i++) {
+    //       const seconds = track.duration
+    //       setDuration([...duration, fmtMSS(seconds)])
+    //     }
+    //   })
+    // }
+    // getTrackLength(tracks)
+    // for (let i = 0; i < audioRef.current.length; i++) {
+    //   getTrackLength(audioRef.current[i])
+    // }
+  }, [])
 
   console.log("Single -> state", state)
   return (
     <MusicPlayerProvider>
       <h2>{name}</h2>
-      <button
-        onClick={() => setState(state => ({ ...state, clicked: "CLICKED" }))}
-      >
-        {state.clicked}
-      </button>
+
       <ul>
         {tracks.map((track, i) => (
           <li key={track._id}>
             <div>
               <strong>{track.name}</strong>
-              <audio
+              {/* <audio
                 ref={el => (audioRef.current[i] = el)}
                 src={track.file}
-                controls
+                controls 
               >
                 <track kind="captions" name={track.name} />
-              </audio>
-              <i>{duration[i]}</i>
+              </audio> */}
+              {/* <i>{duration[i]}</i> */}\
+              <button onClick={() => playTrack(i)}>
+                {currentTrackName === track.name && isPlaying
+                  ? "PAUSE"
+                  : "PLAY"}
+              </button>
+              <p>current track index {currentTrackIndex}</p>
+              <p>{state.duration}</p>
             </div>
           </li>
         ))}
--- www/src/hooks/useMusicPlayer.js
+++ www/src/hooks/useMusicPlayer.js
@@ -4,6 +4,51 @@ import { MusicPlayerContext } from "../context/MusicPlayerContext"
 const useMusicPlayer = () => {
   const [state, setState] = useContext(MusicPlayerContext)
 
+  function fmtMSS(s) {
+    s = Math.floor(s)
+    return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s
+  }
+
+  function makeAudio(tracks) {
+    const ARR = new Array()
+    for (let i = 0; i <= tracks.length; i++) {
+      const au = document.createElement("audio")
+      ARR.push(au)
+    }
+    console.log("makeAudio -> ARR", ARR)
+    tracks.forEach((track, i) => {
+      //   ARR[i] = document.createElement("audio")
+      //   console.log("ARR is " + ARR[i])
+      ARR[i].src = track.file
+      ARR[i].addEventListener("loadedmetadata", function () {
+        setState(state => ({
+          ...state,
+          tracks: state.tracks.concat(fmtMSS(ARR[i].duration)),
+        }))
+      })
+    })
+  }
+  //   function makeAudio(tracks) {
+  //     for (let i = 0; i <= tracks.length; i++) {
+  //       const auARR = new Array(tracks.length)
+  //       if (tracks.length) {
+  //         auARR.push(tracks[i])
+  //       }
+  //       console.log(auARR)
+  //     }
+  //   }
+
+  //) for (let i = 0; i <= tracks.length; i++) {
+  //   console.log(track.audioPlayer)
+  // }
+
+  // tracks.map((track, i) =>
+  // console.log("getTrackLength -> track, i", track, i)
+
+  //   state.audioPlayer[i].addEventListener("loadedmetadata", function () {
+  //     const seconds = state.audioPlayer[i].duration
+  //     setState(state => ({ ...state, duration: fmtMSS(seconds) }))
+
   function playTrack(index) {
     if (index === state.currentTrackIndex) {
       togglePlay()
@@ -19,6 +64,14 @@ const useMusicPlayer = () => {
     }
   }
 
+  function stopPlay() {
+    if (state.isPlaying) {
+      state.audioPlayer.pause()
+    } else {
+      return
+    }
+  }
+
   function togglePlay() {
     if (state.isPlaying) {
       state.audioPlayer.pause()
@@ -28,13 +81,16 @@ const useMusicPlayer = () => {
     setState(state => ({ ...state, isPlaying: !state.isPlaying }))
   }
   return {
+    stopPlay,
     togglePlay,
     playTrack,
+    currentTrackIndex: state.currentTrackIndex,
     currentTrackName:
       state.currentTrackIndex !== null &&
       state.tracks[state.currentTrackIndex].name,
     isPlaying: state.isPlaying,
     trackList: state.tracks,
+    makeAudio,
   }
 }
 
