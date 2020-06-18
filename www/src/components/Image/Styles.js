d7cf40332643a52520f0a06f7c4114d9fbe28ab4
James <jamesgrubb@mac.com>
2020-06-18 13:59:44 +0100

    scaled image

--- www/src/components/Image/Styles.js
+++ www/src/components/Image/Styles.js
@@ -5,11 +5,13 @@ export default styled(Img)`
   filter: url("#duotone");
   top: 0;
   left: 0;
-  transform: scale(1.3);
-  transform-origin: center center;
   width: 100vw;
   height: 100vh;
   object-fit: cover;
   z-index: 0;
   overflow: hidden;
+  > div {
+    transform: scale(1.3);
+    transform-origin: center center;
+  }
 `
