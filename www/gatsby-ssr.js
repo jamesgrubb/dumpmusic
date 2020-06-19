31a809ce987bc777b31f3ca1b8f13b290855710a
James <jamesgrubb@mac.com>
2020-06-19 16:54:03 +0100

    added Gloabl Styles to gatsby-ssr.js

--- www/gatsby-ssr.js
+++ www/gatsby-ssr.js
@@ -1,9 +1,10 @@
 import React from "react"
 import MusicPlayerProvider from "./src/context/MusicPlayerContext"
 import { MediaContextProvider } from "./src/Media"
+import GlobalStyles from "./src/GlobalStyles"
 
 export const wrapRootElement = ({ element }) => (
   <MediaContextProvider>
-    <MusicPlayerProvider>{element}</MusicPlayerProvider>
+    <MusicPlayerProvider><GlobalStyles />{element}</MusicPlayerProvider>
   </MediaContextProvider>
 )
